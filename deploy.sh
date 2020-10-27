3ec3c0d06170c1e09b5ca349c552cebdc78ab38e
s3bc40 <s.goncalvesclaro@gmail.com>
2020-10-27 15:15:09 +0100

    Avoid readonly for deploy sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:s3bc40/binary-converter.git main:gh-pages
+git push -f git@github.com:s3bc40/binary-converter.git prod-branch:gh-pages
 
 cd -
